f26a918178c2bccb438ae9a3d9c4bbdbe816e8c3
jotkafomat <k-j@xl.wp.pl>
2020-10-16 13:00:22 +0100

    add text field

--- spec/features/hi_you_spec.rb
+++ spec/features/hi_you_spec.rb
@@ -8,11 +8,18 @@ feature "home page says hi" do
     visit("/")
     expect(page).to have_content "Hello there!"
   end
+end
 
-  feature "asking for the name" do
-    scenario "loads the home page and asks for the name" do
-      visit("/")
-      expect(page).to have_content "What's your name?"
+feature "asking for the name" do
+  scenario "loads the home page and asks for the name" do
+    visit("/")
+    expect(page).to have_content "What's your name?"
     end
   end
-end
+
+# feature "asking for the name" do
+#   scenario "loads the home page with empty field for a name" do
+#     visit("/")
+#     expect(page).find_field('First Name').value
+#   end
+# end
